9035dc5c54a93c78ecd8603c8012e8a753f9812e
nozomione <nozomione.online@gmail.com>
2022-04-29 09:17:46 -0400

    add a sample advanced filter object for search test

--- src/search/search.test.js
+++ src/search/search.test.js
@@ -3,7 +3,9 @@ import Refinebio from 'index'
 const api = Refinebio({ verbose: true })
 
 test('get search results for the experiments with advanced filtering', async () => {
-  const getSearchResults = await api.search.filter()
+  const getSearchResults = await api.search.filter({
+    downloadable_organism: ['MUS_MUSCULUS', 'HOMO_SAPIENS']
+  })
 
   expect(getSearchResults.isOk).toBeTruthy()
 })
