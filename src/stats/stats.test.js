d0277a4387ebc58c4edaf5e928d9252d8285770c
nozomione <nozomione.online@gmail.com>
2022-05-02 19:01:53 -0400

    adjust stats test and get method in getActions

--- src/stats/stats.test.js
+++ src/stats/stats.test.js
@@ -2,11 +2,11 @@ import Refinebio from 'index'
 
 const api = Refinebio({ verbose: true })
 
-// test('get stats on the health of the system', async () => {
-//   const getStatsOnHealth = await api.stats.get()
+test('get stats on the health of the system', async () => {
+  const getStatsOnHealth = await api.stats.get()
 
-//   expect(getStatsOnHealth.isOk).toBeTruthy()
-// })
+  expect(getStatsOnHealth.isOk).toBeTruthy()
+})
 
 test('get stats on a failure downloader list', async () => {
   const getFailureDownloader = await api.stats.filter.downloader()
--- src/utils/getActions.js
+++ src/utils/getActions.js
@@ -7,7 +7,10 @@ export const getActions = (config, path, pk = 'id') => {
         method: 'POST',
         body: JSON.stringify(data)
       }),
-    get: (id) => makeRequest(config, `${path}/${id}`),
+    get: (id) =>
+      id
+        ? makeRequest(config, `${path}/${id}`)
+        : makeRequest(config, `${path}`),
     filter: (query = {}) => makeRequest(config, `${path}`, query),
     update: (data) => {
       return makeRequest(config, `${path}/${data[pk]}`, {
