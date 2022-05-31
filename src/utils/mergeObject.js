b16dae0e5a2cc615ed31ee87fc9e799665f4381d
nozomione <nozomione.online@gmail.com>
2022-05-31 14:39:27 -0400

    create makeDeepCopy helper, refactor mergeObject helper

--- src/utils/mergeObject.js
+++ src/utils/mergeObject.js
@@ -1,22 +1,11 @@
-export const mergeObject = (source, changes, clone = false) => {
-  if (clone) {
-    return { ...source, ...changes }
-  }
-  if (!changes) {
+import { makeDeepCopy } from 'utils/makeDeepCopy'
+
+export const mergeObject = (source, changes) => {
+  if (!Object.keys(changes).length === 0) {
     return source
   }
 
-  const modifiedSource = source
-
-  Object.keys(changes).forEach((key) => {
-    if (!Object.prototype.hasOwnProperty.call(source, key)) {
-      console.error(`${key} is an invalid key`)
-    }
-
-    modifiedSource[key] = changes[key]
-  })
-
-  return modifiedSource
+  return makeDeepCopy(source, changes)
 }
 
 export default mergeObject
