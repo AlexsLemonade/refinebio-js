c02a119749b4ace076c35588c8df8d2509e3b266
nozomione <nozomione.online@gmail.com>
2022-06-01 17:35:14 -0400

    refactor makeDeepCopy to take a single object and perform deep copy for multi-level nested objects and accessors

--- src/utils/makeDeepCopy.js
+++ src/utils/makeDeepCopy.js
@@ -1,12 +1,31 @@
-export const makeDeepCopy = (...objs) => {
+/*
+@param obj - a source object to be deep copied
+Return valie: a new object
+*/
+
+// TODO: create a datatype check helper
+const isObject = (prop) => typeof prop === 'object'
+const isArray = (arr) => Array.isArray(arr)
+const isAccessor = (obj, prop) =>
+  typeof Object.getOwnPropertyDescriptor(obj, prop).get === 'function' ||
+  typeof Object.getOwnPropertyDescriptor(obj, prop).set === 'function'
+
+export const makeDeepCopy = (source) => {
   const newObj = {}
-  for (const obj of objs) {
-    const keys = Object.keys(obj)
-    keys.forEach((key) => {
-      const descriptor = Object.getOwnPropertyDescriptor(obj, key)
-      Object.defineProperty(newObj, key, descriptor)
-    })
-  }
+
+  if (isArray(source)) return Object.values(source)
+
+  Object.keys(source).forEach((key) => {
+    if (isAccessor(source, key)) {
+      const accessor = Object.getOwnPropertyDescriptor(source, key)
+      Object.defineProperty(newObj, key, accessor)
+    } else {
+      newObj[key] = isObject(source[key])
+        ? makeDeepCopy(source[key])
+        : source[key]
+    }
+  })
+
   return newObj
 }
 
