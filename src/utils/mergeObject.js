f4a7501dae4a2272d94370405d9b16284248879d
nozomione <nozomione.online@gmail.com>
2022-06-06 20:13:09 -0400

    refactor mergeObject and add its unit test

--- src/utils/mergeObject.js
+++ src/utils/mergeObject.js
@@ -1,18 +1,51 @@
+import { isArray, isObject } from 'utils/dataTypeChecker'
 import { makeDeepCopy } from 'utils/makeDeepCopy'
+import { unionArray } from 'utils/unionArray'
+import { sortArray } from 'utils/sortArray'
 
 /*
 @name mergeObject 
-@description merges two objects(if a second argument is passed) and returns a new merged object. If no second argument, then returns a first argument
+@description merges two objects and returns a new merged object 
+ - If no second argument was passed, then it returns the first argument
+ - If both objects have the same keys with the array datatyp, then it returns a union of those arrays
+   e.g) {a: [1,2]}, {a: [2,3]} should be {a: [1,2,3]}
 @param {object} base - a base object whose properties get overwriten by a change object if any
-@param {object} change - an object which gets merged into a base object
-@return {object} - a new object or a base object if no change object was passed
+@param {object} override - an object which gets merged into a base object
+@return {object} a new object or a base object if no second argument
 */
-export const mergeObject = (base, change) => {
-  if (!Object.keys(change).length === 0) {
-    return base
-  }
-  // TODO: it should combine values instead of overwriting(e.g: base {a: [1,2]} and change { a: [2,3]} should produce { a: [1, 2, 3])
-  const newObj = { ...makeDeepCopy(base), ...makeDeepCopy(change) }
+export const mergeObject = (base, override) => {
+  if (!override) return base
+
+  const keys = Object.keys(override)
+  const newObj = makeDeepCopy(base)
+  const overrideCopy = makeDeepCopy(override)
+
+  keys.forEach((key) => {
+    if (!isObject(newObj[key])) {
+      if (!newObj[key] || newObj[key] !== overrideCopy[key]) {
+        newObj[key] = overrideCopy[key]
+      }
+    }
+
+    if (isArray(newObj[key]) && isArray(overrideCopy[key])) {
+      newObj[key] = sortArray(unionArray(newObj[key], overrideCopy[key]))
+    } else if (isObject(newObj[key]) && isObject(overrideCopy[key])) {
+      const newObjKeys = Object.keys(newObj[key])
+      newObjKeys.forEach((newObjKey) => {
+        if (
+          isArray(newObj[key][newObjKey]) &&
+          isArray(overrideCopy[key][newObjKey])
+        ) {
+          overrideCopy[key][newObjKey] = sortArray(
+            unionArray(newObj[key][newObjKey], overrideCopy[key][newObjKey])
+          )
+        }
+
+        newObj[key] = { ...newObj[key], ...overrideCopy[key] }
+        mergeObject(newObj[key], overrideCopy[key])
+      })
+    }
+  })
 
   return newObj
 }
--- src/utils/mergeObject.test.js
+++ src/utils/mergeObject.test.js
@@ -1,13 +1,82 @@
-import Refinebio from 'index'
+import { mergeObject } from 'utils/mergeObject'
 
-const api = Refinebio()
+describe('Merge two objets', () => {
+  const input = [
+    [{ a: 'a' }],
+    [
+      {
+        a: 'a1',
+        b: [1, 2, 5],
+        c: {
+          d: [1, 2],
+          e: [2, 3],
+          f: [3, 4],
+          g: [5, 6],
+          h: [7, 8]
+        }
+      },
+      {
+        a: 'a2',
+        b: [2, 3, 4],
+        c: {
+          d: [2, 3],
+          e: [4, 5],
+          f: [6, 7],
+          g: [8, 9]
+        }
+      }
+    ],
+    [
+      {
+        data: {
+          'E-ABC-1': ['SAMP1', 'SAMP2'],
+          'E-ABC-2': ['SAMP1', 'SAMP2'],
+          'E-ABC-3': ['SAMP1', 'SAMP2']
+        },
+        email: '',
+        aggregate_by: 'ALL',
+        scale_by: 'NONE',
+        quantile_normalize: true,
+        quant_sf_only: false,
+        svd_algorithm: 'NONE'
+      },
+      {
+        email: 'jonedoe@test.com',
+        aggregate_by: 'EXPERIMENT',
+        data: {
+          'E-ABC-1': ['SAMP2', 'SAMP3', 'SAMP4'],
+          'E-ABC-2': ['SAMP1', 'SAMP2'],
+          'E-ABC-4': ['SAMP3', 'SAMP4']
+        }
+      }
+    ]
+  ]
 
-test('update the config', () => {
-  const modifiedConfig = api.updateConfig({
-    path: `https://api.staging.refine.bio/v1/`,
-    verbose: true
+  test('should return the first argument', () => {
+    const output = mergeObject(input[0][0])
+    expect(output).toBe(input[0][0])
   })
 
-  expect(modifiedConfig.path).toBe(`https://api.staging.refine.bio/v1/`)
-  expect(modifiedConfig.verbose).toBe(true)
+  test('should return a new merged object', () => {
+    const outputOne = mergeObject(input[1][0], input[1][1])
+    expect(outputOne.a).toBe('a2')
+    expect(JSON.stringify(outputOne.b)).toBe(JSON.stringify([1, 2, 3, 4, 5]))
+    expect(JSON.stringify(outputOne.c.d)).toBe(JSON.stringify([1, 2, 3]))
+
+    const outputTwo = mergeObject(input[2][0], input[2][1])
+    expect(outputTwo.email).toBe('jonedoe@test.com')
+    expect(outputTwo.aggregate_by).toBe('EXPERIMENT')
+    expect(outputTwo.scale_by).toBe('NONE')
+    expect(JSON.stringify(outputTwo.data['E-ABC-1'])).toBe(
+      JSON.stringify(['SAMP1', 'SAMP2', 'SAMP3', 'SAMP4'])
+    )
+    expect(JSON.stringify(outputTwo.data['E-ABC-3'])).toBe(
+      JSON.stringify(['SAMP1', 'SAMP2'])
+    )
+    expect(outputTwo.data['E-ABC-3']).not.toBe(input[2][0].data['E-ABC-3'])
+    expect(JSON.stringify(outputTwo.data['E-ABC-4'])).toBe(
+      JSON.stringify(['SAMP3', 'SAMP4'])
+    )
+    expect(outputTwo.data['E-ABC-4']).not.toBe(input[2][1].data['E-ABC-4'])
+  })
 })
