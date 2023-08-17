444eff3ab4f11e2004ed29f215f105bdf488a3b7
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-17 14:23:09 -0400

    52 - Resolve Camel Case Linting Warnings in Tests (#53)
    * move all the resource files to a 'resources' folder and fix imports
    
    * remove duplicated files
    
    * add Dataset helper to the root index
    
    * create makeDeepCopy helper, refactor mergeObject helper
    
    * create Dataset helper(in progress), add enums for available options
    
    * remove validations and enums, add publicInterface, refactor all mutators to return publicInterface
    
    * refactor makeDeepCopy to take a single object and perform deep copy for multi-level nested objects
    
    * use the mergeObject helper instead of the makeDeepCopy helper
    
    * (already merged change) match a primary key value with a name of identifier
    
    * Changes include:
    - remove edge cases from publicInterface remove* methods
    - reorder publicInterface to be alphabetical order for import and export
    
    * delete mergeObject and replace with mergeDatasets
    
    * create a separate export for Dataset helper and remove it from default export
    
    * rename vars - 'settings' to 'override', 'configurable' to 'defaults'
    
    * Update includes:
    - add mapObject helper in 'utils/'
    - create an instance of publicInterface using mapObject instead of mutating it directly(in Dataset)
    - revise each publicInterface helper accordingly
    
    * overwrite model object(dataset) with api response object
    
    * use deepCopy for copying model.data
    
    * revise mapObject helper and update Detaset accordingly
    
    * (clean up) delete makeDeepCopy helper - no longer needed
    
    * (minor update) revise helper annotations
    
    * fix minor typo in annotations
    
    * fix broken tests
    
    * (edit) add paths for resources and dataset directories in webpack.config
    
    * clean up the code blocks related to the 'delete' method
    
    * camel case the indentifiers in tests

--- src/resources/samples/samples.test.js
+++ src/resources/samples/samples.test.js
@@ -13,8 +13,8 @@ test('get detailed information on samples with filtering', async () => {
 })
 
 test('get sample details by its accession code', async () => {
-  const { accession_code } = filterRequest.response.results[0]
-  const getSample = await api.samples.get(accession_code)
+  const { accession_code: accessionCode } = filterRequest.response.results[0]
+  const getSample = await api.samples.get(accessionCode)
 
   expect(getSample.isOk).toBeTruthy()
 })
