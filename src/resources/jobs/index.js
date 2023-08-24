a2b4aa1e9cff4b52efe6ae2608d885249e0777e0
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-24 10:52:55 -0400

    (edit) optimize the resource jobs and its test

--- src/resources/jobs/index.js
+++ src/resources/jobs/index.js
@@ -3,27 +3,15 @@ import { getActions } from 'utils/getActions'
 import { availableActions } from 'utils/availableActions'
 
 export const jobs = (config = defaultConfig) => {
-  const downloaderGet = getActions(config, 'jobs/downloader').get
-  const processorGet = getActions(config, 'jobs/processor').get
-  const surveyGet = getActions(config, 'jobs/survey').get
-  const downloaderFilter = getActions(config, 'jobs/downloader/').filter
-  const processorFilter = getActions(config, 'jobs/processor/').filter
-  const surveyFilter = getActions(config, 'jobs/survey/').filter
+  const get = async (name, id = '') => {
+    const { get: getJob } = getActions(config, `jobs/${name}`, id)
+
+    return getJob(id)
+  }
 
   return {
     ...availableActions('jobs'),
-    downloader: {
-      get: downloaderGet,
-      filter: downloaderFilter
-    },
-    processor: {
-      get: processorGet,
-      filter: processorFilter
-    },
-    survey: {
-      get: surveyGet,
-      filter: surveyFilter
-    }
+    get
   }
 }
 
--- src/resources/jobs/jobs.test.js
+++ src/resources/jobs/jobs.test.js
@@ -3,57 +3,57 @@ import Refinebio from 'index'
 const api = Refinebio({ verbose: true })
 
 describe('downloader job', () => {
-  let filterRequest
+  let getRequest
 
   beforeEach(async () => {
-    filterRequest = await api.jobs.downloader.filter({})
+    getRequest = await api.jobs.get('downloader')
   })
 
-  test('get a list of downloader jobs with filtering', async () => {
-    expect(filterRequest.isOk).toBeTruthy()
+  test('get a list of downloader jobs', async () => {
+    expect(getRequest.isOk).toBeTruthy()
   })
 
   test('get a downloader job', async () => {
-    const { id } = filterRequest.response.results[0]
-    const getDownloaderJob = await api.jobs.downloader.get(id)
+    const { id } = getRequest.response.results[0]
+    const getDownloaderJob = await api.jobs.get('downloader', id)
 
     expect(getDownloaderJob.isOk).toBeTruthy()
   })
 })
 
 describe('processor job', () => {
-  let filterRequest
+  let getRequest
 
   beforeEach(async () => {
-    filterRequest = await api.jobs.processor.filter({})
+    getRequest = await api.jobs.get('processor')
   })
 
-  test('get a list of processor jobs with filtering', async () => {
-    expect(filterRequest.isOk).toBeTruthy()
+  test('get a list of processor jobs', async () => {
+    expect(getRequest.isOk).toBeTruthy()
   })
 
   test('get a processor job', async () => {
-    const { id } = filterRequest.response.results[0]
-    const getProcessorJob = await api.jobs.processor.get(id)
+    const { id } = getRequest.response.results[0]
+    const getProcessorJob = await api.jobs.get('processor', id)
 
     expect(getProcessorJob.isOk).toBeTruthy()
   })
 })
 
 describe('suevey job', () => {
-  let filterRequest
+  let getRequest
 
   beforeEach(async () => {
-    filterRequest = await api.jobs.survey.filter({})
+    getRequest = await api.jobs.get('survey')
   })
 
-  test('get a list of survey jobs with filtering', async () => {
-    expect(filterRequest.isOk).toBeTruthy()
+  test('get a list of survey jobs', async () => {
+    expect(getRequest.isOk).toBeTruthy()
   })
 
   test('get a survey job', async () => {
-    const { id } = filterRequest.response.results[0]
-    const getSurveyJob = await api.jobs.survey.get(id)
+    const { id } = getRequest.response.results[0]
+    const getSurveyJob = await api.jobs.get('survey', id)
 
     expect(getSurveyJob.isOk).toBeTruthy()
   })
