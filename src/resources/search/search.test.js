9a10b87edc96bd633bd7b9a415d3308171b046c8
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-24 10:56:38 -0400

    (edit) make minor optimization to the resource search

--- src/resources/search/search.test.js
+++ src/resources/search/search.test.js
@@ -3,9 +3,10 @@ import Refinebio from 'index'
 const api = Refinebio({ verbose: true })
 
 test('get search results for the experiments with advanced filtering', async () => {
-  const getSearchResults = await api.search.filter({
+  const searchQuery = {
     downloadable_organism: ['MUS_MUSCULUS', 'HOMO_SAPIENS']
-  })
+  }
+  const getSearchResults = await api.search.filter(searchQuery)
 
   expect(getSearchResults.isOk).toBeTruthy()
 })
